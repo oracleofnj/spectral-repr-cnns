13b6ce9c9456500d38f89d0c46c6bd7fc831fff7
Alex Wainger <alex_wainger@brown.edu>
2017-12-15 10:18:45 -0500

    Proper gradients, l2 loss, full architecture for deep and generic

--- src/modules/cnn_with_spectral_parameterization.py
+++ src/modules/cnn_with_spectral_parameterization.py
@@ -1,17 +1,21 @@
-from .layers import spectral_conv_layer, fc_layer
+from .layers import spectral_conv_layer
 import numpy as np
 import tensorflow as tf
 
 class CNN_Spectral_Param():
 	def __init__(self,
-		num_output,
+		num_output=10,
 		architecture='generic',
+		use_spectral_params=True,
+		kernel_size=3,
 		l2_norm=0.01,
-		learning_rate=1e-3,
+		learning_rate=1e-4,
 		random_seed=0):
 
 		self.num_output = num_output
 		self.architecture = architecture
+		self.use_spectral_params = use_spectral_params
+		self.kernel_size = kernel_size
 		self.l2_norm = l2_norm
 		self.learning_rate = learning_rate
 		self.random_seed = random_seed
@@ -32,7 +36,6 @@ class CNN_Spectral_Param():
 
 	def evaluate(self, pred, input_y):
 		with tf.name_scope('evaluate'):
-			# pred = tf.argmax(output, axis=1)
 			error_num = tf.count_nonzero(pred - input_y, name='error_num')
 			tf.summary.scalar('LeNet_error_num', error_num)
 		return error_num
@@ -46,6 +49,7 @@ class CNN_Spectral_Param():
 			ys = tf.placeholder(shape=[None, ], dtype=tf.int64)
 
 			output, loss = self.build_graph(xs, ys)
+
 			iters = int(X_train.shape[0] / batch_size)
 			print('number of batches for training: {}'.format(iters))
 
@@ -65,7 +69,9 @@ class CNN_Spectral_Param():
 
 					for itr in range(iters):
 						iter_total += 1
-						print("Training batch {0}".format(itr))
+
+						# print(sess.run(tf.get_default_graph().get_tensor_by_name('spec_conv_layer_1/real_1:0')[:,:,0,3]))
+						# print(sess.run(tf.get_default_graph().get_tensor_by_name('spec_conv_layer_1/imag_1:0')[:,:,0,3]))
 
 						training_batch_x = X_train[itr * batch_size:
 												   (1 + itr) * batch_size]
@@ -78,75 +84,241 @@ class CNN_Spectral_Param():
 													   ys: training_batch_y})
 						self.loss_vals.append(cur_loss)
 						self.train_accuracy.append(1 - train_eve / batch_size)
+
+						if self.use_spectral_params:
+							self._correct_spectral_weights()
+
 					print(self.train_accuracy[-1])
+					print(self.loss_vals[-1])
+
+	def _correct_spectral_weights(self):
+		pass
+
 
 	def _build_generic_architecture(self, input_x, input_y):
+		spatial_conv_weights = []
 
-		conv1 = spectral_conv_layer(input_x=input_x,
-									in_channel=input_x.shape[-1],
-									out_channel=96,
-									kernel_shape=3,
-									random_seed=self.random_seed,
-									m=1)
+		if self.use_spectral_params:
+			sc_layer = spectral_conv_layer(input_x=input_x,
+										in_channel=3,
+										out_channel=96,
+										kernel_size=self.kernel_size,
+										random_seed=self.random_seed,
+										m=1)
+			conv1 = sc_layer.output()
+			spatial_conv_weights.append(sc_layer.weight)
 
-		pool1_output = tf.layers.max_pooling2d(inputs=conv1.output(),
+		else:
+			conv1 = tf.layers.conv2d(inputs=input_x,
+										filters=96,
+										kernel_size=self.kernel_size,
+										activation=tf.nn.relu,
+										name='conv1')
+
+		pool1 = tf.layers.max_pooling2d(inputs=conv1,
 										pool_size=3,
 										strides=2,
 										padding='SAME',
 										name='max_pool_1')
 
-		conv2 = spectral_conv_layer(input_x=pool1_output,
-									in_channel=pool1_output.shape[-1],
-									out_channel=192,
-									kernel_shape=3,
-									random_seed=self.random_seed,
-									m=2)
+		if self.use_spectral_params:
+			sc_layer = spectral_conv_layer(input_x=pool1,
+										in_channel=96,
+										out_channel=192,
+										kernel_size=self.kernel_size,
+										random_seed=self.random_seed,
+										m=2)
+			conv2 = sc_layer.output()
+			spatial_conv_weights.append(sc_layer.weight)
 
-		pool2_output = tf.layers.max_pooling2d(inputs=conv2.output(),
+		else:
+			conv2 = tf.layers.conv2d(inputs=pool1,
+										filters=192,
+										kernel_size=self.kernel_size,
+										activation=tf.nn.relu,
+										name='conv2')
+
+		pool2 = tf.layers.max_pooling2d(inputs=conv2,
 										pool_size=3,
 										strides=2,
 										padding='SAME',
 										name='max_pool_2')
 
-		pool_shape = pool2_output.get_shape()
-		img_vector_length = pool_shape[1].value * pool_shape[2].value * pool_shape[3].value
-		flatten = tf.reshape(pool2_output, shape=[-1, img_vector_length])
+		flatten = tf.contrib.layers.flatten(inputs=pool2)
 
-		fc1 = fc_layer(input_x=flatten,
-						in_size=img_vector_length,
-						out_size=1024,
-						rand_seed=self.random_seed,
-						activation_function=tf.nn.relu,
-						m=1)
+		fc1 = tf.contrib.layers.fully_connected(inputs=flatten,
+												num_outputs=1024,
+												activation_fn=tf.nn.relu)
+		fc2 = tf.contrib.layers.fully_connected(inputs=fc1,
+												num_outputs=512,
+												activation_fn=tf.nn.relu)
+		fc3 = tf.contrib.layers.fully_connected(inputs=fc2,
+												num_outputs=self.num_output,
+												activation_fn=None)
 
-		fc2 = fc_layer(input_x=fc1.output(),
-						in_size=1024,
-						out_size=512,
-						rand_seed=self.random_seed,
-						activation_function=tf.nn.relu,
-						m=2)
-
-		fc3 = fc_layer(input_x=fc2.output(),
-						in_size=512,
-						out_size=self.num_output,
-						rand_seed=self.random_seed,
-						activation_function=None,
-						m=3)
-
-		output = tf.nn.softmax(fc3.output())
+		fc_weights = [v for v in tf.trainable_variables() if 'weights' in v.name]
 
 		with tf.name_scope("loss"):
-			# l2_loss = tf.reduce_sum([tf.norm(w) for w in [fc1.weight, fc2.weight, fc3.weight]])
-			# l2_loss += tf.reduce_sum([tf.norm(w, axis=[-2, -1]) for w in [conv1.weight, conv2.weight]])
+			if self.use_spectral_params:
+				l2_loss = tf.reduce_sum([tf.norm(w, axis=[-2, -1]) for w in spatial_conv_weights])
+			else:
+				conv_kernels = [v for v in tf.trainable_variables() if 'kernel' in v.name]
+				l2_loss = tf.reduce_sum([tf.norm(w, axis=[-2, -1]) for w in conv_kernels])
 
+			l2_loss = tf.reduce_sum([tf.norm(w) for w in fc_weights])
+			
 			label = tf.one_hot(input_y, self.num_output)
 			cross_entropy_loss = tf.reduce_mean(
-				tf.nn.softmax_cross_entropy_with_logits(labels=label,logits=output),
+				tf.nn.softmax_cross_entropy_with_logits(labels=label, logits=fc3),
 				name='cross_entropy')
-			loss = cross_entropy_loss
-			# loss = tf.add(cross_entropy_loss,self.l2_norm * l2_loss,name='loss')
+			loss = tf.add(cross_entropy_loss, self.l2_norm * l2_loss, name='loss')
 
-		return output, loss
+		return fc3, loss
 
 	def _build_deep_architecture(self, input_x, input_y):
-		pass
+		spatial_conv_weights = []
+
+		if self.use_spectral_params:
+			sc_layer = spectral_conv_layer(input_x=input_x,
+											in_channel=3,
+											out_channel=96,
+											kernel_size=self.kernel_size,
+											random_seed=self.random_seed,
+											m=1)
+			conv1 = sc_layer.output()
+			spatial_conv_weights.append(sc_layer.weight)
+		else:
+			conv1 = tf.layers.conv2d(inputs=input_x,
+										filters=96,
+										kernel_size=self.kernel_size,
+										activation=tf.nn.relu,
+										name='conv1')
+
+		if self.use_spectral_params:
+			sc_layer = spectral_conv_layer(input_x=conv1,
+											in_channel=96,
+											out_channel=96,
+											kernel_size=self.kernel_size,
+											random_seed=self.random_seed,
+											m=2)
+			conv2 = sc_layer.output()
+			spatial_conv_weights.append(sc_layer.weight)
+		else:
+			conv2 = tf.layers.conv2d(inputs=conv1,
+										filters=96,
+										kernel_size=self.kernel_size,
+										activation=tf.nn.relu,
+										name='conv2')
+
+		pool1 = tf.layers.max_pooling2d(inputs=conv2,
+										pool_size=3,
+										strides=2,
+										padding='SAME',
+										name='max_pool_1')
+
+		if self.use_spectral_params:
+			sc_layer = spectral_conv_layer(input_x=pool1,
+											in_channel=96,
+											out_channel=192,
+											kernel_size=self.kernel_size,
+											random_seed=self.random_seed,
+											m=3)
+			conv3 = sc_layer.output()
+			spatial_conv_weights.append(sc_layer.weight)
+		else:
+			conv3 = tf.layers.conv2d(inputs=pool1,
+										filters=192,
+										kernel_size=self.kernel_size,
+										activation=tf.nn.relu,
+										name='conv3')
+
+		if self.use_spectral_params:
+			sc_layer = spectral_conv_layer(input_x=conv3,
+											in_channel=192,
+											out_channel=192,
+											kernel_size=self.kernel_size,
+											random_seed=self.random_seed,
+											m=4)
+			conv4 = sc_layer.output()
+			spatial_conv_weights.append(sc_layer.weight)
+		else:
+			conv4 = tf.layers.conv2d(inputs=conv3,
+										filters=192,
+										kernel_size=self.kernel_size,
+										activation=tf.nn.relu,
+										name='conv4')
+
+		if self.use_spectral_params:
+			sc_layer = spectral_conv_layer(input_x=conv4,
+											in_channel=192,
+											out_channel=192,
+											kernel_size=self.kernel_size,
+											random_seed=self.random_seed,
+											m=5)
+			conv5 = sc_layer.output()
+			spatial_conv_weights.append(sc_layer.weight)
+		else:
+			conv5 = tf.layers.conv2d(inputs=conv4,
+										filters=192,
+										kernel_size=self.kernel_size,
+										activation=tf.nn.relu,
+										name='conv5')
+
+		pool2 = tf.layers.max_pooling2d(inputs=conv5,
+										pool_size=3,
+										strides=2,
+										padding='SAME',
+										name='max_pool_2')
+
+		if self.use_spectral_params:
+			sc_layer = spectral_conv_layer(input_x=input_x,
+											in_channel=192,
+											out_channel=192,
+											kernel_size=1,
+											random_seed=self.random_seed,
+											m=6)
+			conv6 = sc_layer.output()
+			spatial_conv_weights.append(sc_layer.weight)
+		else:
+			conv6 = tf.layers.conv2d(inputs=pool2_output,
+										filters=192,
+										kernel_size=1,
+										activation=tf.nn.relu,
+										name='conv6')
+
+		if self.use_spectral_params:
+			sc_layer = spectral_conv_layer(input_x=conv6,
+											in_channel=192,
+											out_channel=10,
+											kernel_size=1,
+											random_seed=self.random_seed,
+											m=7)
+			conv7 = sc_layer.output()
+			spatial_conv_weights.append(sc_layer.weight)
+		else:
+			conv7 = tf.layers.conv2d(inputs=conv6,
+										filters=10,
+										kernel_size=1,
+										activation=None,
+										name='conv7')
+
+		global_avg = tf.reduce_mean(input_tensor=conv7, axis=[1,2])
+
+		with tf.name_scope("loss"):
+			if self.use_spectral_params:
+				l2_loss = tf.reduce_sum([tf.norm(w, axis=[-2, -1]) for w in spatial_conv_weights])
+			else:
+				conv_kernels = [v for v in tf.trainable_variables() if 'kernel' in v.name]
+				l2_loss = tf.reduce_sum([tf.norm(w, axis=[-2, -1]) for w in conv_kernels])
+
+			l2_loss = tf.reduce_sum([tf.norm(w) for w in fc_weights])
+			
+			label = tf.one_hot(input_y, self.num_output)
+			cross_entropy_loss = tf.reduce_mean(
+				tf.nn.softmax_cross_entropy_with_logits(labels=label, logits=global_avg),
+				name='cross_entropy')
+			loss = tf.add(cross_entropy_loss, self.l2_norm * l2_loss, name='loss')
+
+		return global_avg, loss
+
+
--- src/modules/layers.py
+++ src/modules/layers.py
@@ -206,16 +206,21 @@ class spectral_pool_layer(object):
         mat2 = self._tfshift(mat, n, 0, invert=True)
         return mat2
 
+def _glorot_sample(kernel_size, n_in, n_out, random_seed):
+        np.random.seed(random_seed)
+        limit = np.sqrt(6 / (n_in + n_out))
+        return np.random.uniform(low=-limit, high=limit, size=(n_in, n_out, kernel_size, kernel_size))
+
 class spectral_conv_layer(object):
     def __init__(self, input_x, in_channel, out_channel,
-                 kernel_shape, random_seed, m=0):
+                 kernel_size, random_seed, m=0):
         """
         NOTE: Image should be CHANNEL LAST
         :param input_x: Should be a 4D array like:
                             (batch_num, channel_num, img_len, img_len)
         :param in_channel: The number of channels
         :param out_channel: number of filters required
-        :param kernel_shape: kernel size
+        :param kernel_size: kernel size
         :param random_seed: random seed
         :param index: The layer index used for naming
         """
@@ -225,17 +230,19 @@ class spectral_conv_layer(object):
 
         with tf.variable_scope('spec_conv_layer_{0}'.format(m)):
             with tf.name_scope('spec_conv_kernel'):
-                w_shape = [kernel_shape, kernel_shape, in_channel, out_channel]
-                spatial_weight_init = tf.get_variable(
-                    name='spatial_weight_init_{0}'.format(m),
-                    shape=w_shape,
-                    initializer=tf.glorot_uniform_initializer(seed=random_seed))
+                samp = _glorot_sample(kernel_size, in_channel, out_channel, random_seed)
+                spectral_weight_init = tf.transpose(tf.fft2d(samp), [2,3,0,1])
 
-                spectral_weight_vals = tf.fft2d(tf.cast(spatial_weight_init.initialized_value(), tf.complex64))
-                spectral_weight = tf.get_variable(
-                    name='spectral_weight_{0}'.format(m),
-                    initializer=spectral_weight_vals)
-                self.weight = spectral_weight
+                real_init = tf.get_variable(
+                    name='real_{0}'.format(m),
+                    initializer=tf.real(spectral_weight_init))
+
+                imag_init = tf.get_variable(
+                    name='imag_{0}'.format(m),
+                    initializer=tf.imag(spectral_weight_init))
+
+                spectral_weight = tf.complex(real_init, imag_init, name='spectral_weight_{0}'.format(m))
+                self.spectral_weight = spectral_weight
 
             with tf.variable_scope('conv_bias'):
                 b_shape = [out_channel]
@@ -245,20 +252,14 @@ class spectral_conv_layer(object):
                     initializer=tf.glorot_uniform_initializer(seed=random_seed))
                 self.bias = bias
 
-            # strides [1, x_movement, y_movement, 1]
-            spatial_weight = tf.cast(tf.ifft2d(spectral_weight), tf.float32, name='spatial_weight_{0}'.format(m))
+            complex_spatial_weight = tf.transpose(tf.ifft2d(tf.transpose(spectral_weight, [2,3,0,1])), [2,3,0,1])
+            spatial_weight = tf.real(complex_spatial_weight, name='spatial_weight_{0}'.format(m))
+            self.weight = spatial_weight
+
             conv_out = tf.nn.conv2d(input_x, spatial_weight,
                                     strides=[1, 1, 1, 1],
                                     padding="SAME")
-            cell_out = tf.nn.relu(conv_out + bias)
-
-            self.cell_out = cell_out
-
-            print(spatial_weight)
-            print(spectral_weight)
-
-            # tf.summary.histogram('conv_layer/{}/kernel'.format(m), weight)
-            # tf.summary.histogram('conv_layer/{}/bias'.format(m), bias)
+            self.cell_out = tf.nn.relu(conv_out + bias)
 
     def output(self):
         return self.cell_out
