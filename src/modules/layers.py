473216d8720302c1c7c1fd7fd74bebe2af4d91ec
oracleofnj <jared.samet@aya.yale.edu>
2017-12-16 17:17:50 -0500

    Add comments, clean up

--- src/modules/layers.py
+++ src/modules/layers.py
@@ -162,12 +162,17 @@ class spectral_pool_layer(object):
         assert H == W
 
         with tf.variable_scope('spectral_pool_layer_{0}'.format(m)):
+            # Compute the Fourier transform of the image
             im_fft = tf.fft2d(tf.cast(input_x, tf.complex64))
+
+            # Truncate the spectrum
             im_transformed = _common_spectral_pool(im_fft, filter_size)
             if (
                 freq_dropout_lower_bound is not None and
                 freq_dropout_upper_bound is not None
             ):
+                # If we are in the training phase, we need to drop all
+                # frequencies above a certain randomly determined level.
                 def true_fn():
                     tf_random_cutoff = tf.random_uniform(
                         [],
@@ -180,6 +185,8 @@ class spectral_pool_layer(object):
                     )
                     return im_transformed * dropout_mask
 
+                # In the testing phase, return the truncated frequency
+                # matrix unchanged.
                 def false_fn():
                     return im_transformed
 
@@ -198,17 +205,6 @@ class spectral_pool_layer(object):
                 cell_out = im_out
             tf.summary.histogram('sp_layer/{}/activation'.format(m), cell_out)
 
-        # THERE COULD BE A NORMALISING STEP HERE SIMILAR TO BATCH NORM BUT
-        # I'M SKIPPING IT HERE
-        # im_channel_last = tf.transpose(tf.real(tf.ifft2d(im_ishift)),
-        #                                perm=[0, 2, 3, 1])
-
-        # # normalize image:
-        # channel_max = tf.reduce_max(im_channel_last, axis=(0, 1, 2))
-        # channel_min = tf.reduce_min(im_channel_last, axis=(0, 1, 2))
-        # im_out = tf.divide(im_channel_last - channel_min,
-        #                    channel_max - channel_min)
-
         self.cell_out = cell_out
 
     def output(self):
@@ -225,46 +221,6 @@ class spectral_pool_layer(object):
         end = freq_dropout + start
         out[:, start:end, start:end] = 1
         return out
-
-    def _no_dropout_matrix(self):
-        return np.ones(shape=self.fft_shape[1:],
-                       dtype=np.complex64)
-
-    def _tfshift(self, matrix, n, axis=1, invert=False):
-        """Handler for shifting one axis at a time.
-        Helpful for fftshift if invert is False and ifftshift otherwise
-        """
-        if invert:
-            mid = n - (n + 1) // 2
-        else:
-            mid = (n + 1) // 2
-        if axis == 1:
-            start = [0, 0, 0, mid]
-            end = [-1, -1, -1, mid]
-        else:
-            start = [0, 0, mid, 0]
-            end = [-1, -1, mid, -1]
-        out = tf.concat([tf.slice(matrix, start, [-1, -1, -1, -1]),
-                         tf.slice(matrix, [0, 0, 0, 0], end)], axis + 2)
-        return out
-
-    def _tf_fftshift(self, matrix, n):
-        """Performs similar function to numpy's fftshift
-        Note: Takes image as a channel first numpy array of shape:
-            (batch_size, channels, height, width)
-        """
-        mat = self._tfshift(matrix, n, 1)
-        mat2 = self._tfshift(mat, n, 0)
-        return mat2
-
-    def _tf_ifftshift(self, matrix, n):
-        """Performs similar function to numpy's ifftshift
-        Note: Takes image as a channel first numpy array of shape:
-            (batch_size, channels, height, width)
-        """
-        mat = self._tfshift(matrix, n, 1, invert=True)
-        mat2 = self._tfshift(mat, n, 0, invert=True)
-        return mat2
 
 def _glorot_sample(kernel_size, n_in, n_out):
         limit = np.sqrt(6 / (n_in + n_out))
