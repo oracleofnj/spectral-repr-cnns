62e085da7334e1050f442993c80ced1d969d75b9
oracleofnj <jared.samet@aya.yale.edu>
2017-12-16 09:46:54 -0500

    CIFAR 100 is not in batches

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -96,6 +96,60 @@ def load_cifar10(num_batches=5,
     return images, labels, test_images, test_labels
 
 
+def load_cifar100(get_test_data=True,
+                  channels_last=True):
+    """Load the cifar 100 data (not in batches).
+
+    Args:
+        get_test_data: bool, whether to return test data
+    Returns:
+        (images, labels) it get_test_data False
+        (images, labels, test_images, test_labels) otherwise
+        images are numpy arrays of shape:
+                    (num_images, num_channels, width, height)
+        labels are 1D numpy arrays
+    """
+    download_cifar10(download_100=True)
+
+    # load batches in order:
+    dirpath = os.path.join(BASE_DIR, 'cifar-100-python')
+    images = None
+    filename = 'train'
+    fpath = os.path.join(dirpath, filename)
+    with open(fpath, 'rb') as f:
+        content = pickle.load(f, encoding='bytes')
+    if images is None:
+        images = content[b'data']
+        labels = content[b'labels']
+    # convert to labels:
+    labels = np.asarray(labels)
+    # convert to RGB format:
+    images = images.reshape(-1, 3, 32, 32)
+
+    # normalize data by dividing by 255:
+    images = images / 255.
+    if channels_last:
+        images = np.moveaxis(images, 1, -1)
+
+    if not get_test_data:
+        return images, labels
+
+    filename = 'test'
+    fpath = os.path.join(dirpath, filename)
+    with open(fpath, 'rb') as f:
+        content = pickle.load(f, encoding='bytes')
+    test_images = content[b'data'].reshape(-1, 3, 32, 32)
+    test_labels = np.asarray(content[b'labels'])
+
+    # normalize:
+    test_images = test_images / 255.
+    # make channels last:
+    if channels_last:
+        test_images = np.moveaxis(test_images, 1, -1)
+
+    return images, labels, test_images, test_labels
+
+
 def strip_consts(graph_def, max_const_size=32):
     """Strip large constant values from graph_def."""
     strip_def = tf.GraphDef()
